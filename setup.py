d5a9cdf339276e9923b67a355c6de930a7c510b9
Sumeet Chougule <101477214+SumeetChougule@users.noreply.github.com>
2022-10-03 10:02:33 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='Q-balls',
+      version='1.0',
+      description='SKyrme Q_balls',
+      author='Sumeet Chougule',
+      author_email='sumeetchougule@live.com',
+      url='https://github.com/SumeetChougule/Skyrme-Q-balls.git',
+      packages=['distutils', 'distutils.command'],
+     )
